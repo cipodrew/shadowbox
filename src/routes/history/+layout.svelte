7d8f705a95153ba6766de372581d2e582f29ea12
adolci <141414129+cipodrew@users.noreply.github.com>
2024-08-02 18:33:55 +0200

    modifica history page

---
+++ src/routes/history/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+	let { children } = $props();
+
+	/**
+	 *@type {[]}
+	 */
+	let sessions;
+</script>
+
+<div class="columns">
+	<section>
+		<ul>
+			<a href=""> <li>Session of 2024-08-2=02</li></a>
+		</ul>
+	</section>
+	{@render children()}
+</div>
+
+<style>
+	.columns {
+		display: grid;
+		grid-template-columns: 1fr 3fr;
+	}
+</style>
--- src/routes/history/+page.svelte
+++ src/routes/history/+page.svelte
@@ -1,4 +1,20 @@
 <svelte:head>
-    <title>Historycal graph</title>
+	<title>Historycal graph</title>
 </svelte:head>
-<h1>Your past training sessions</h1>
+<div class="wrapper">
+	<h1>Your past training sessions</h1>
+	<div>test</div>
+</div>
+
+<style>
+	.wrapper {
+		display: grid;
+		place-items: center;
+		border-style: solid;
+		border-width: 5px;
+		border-color: --var(--border);
+		/*grid-template-rows: minmax(100px, auto) 1fr;*/
+		height: 100svh;
+		/*overflow: hidden;*/
+	}
+</style>
