630a07d0adbda57d9fe50c8e00c09acafbc0ae02
adolci <141414129+cipodrew@users.noreply.github.com>
2024-08-05 00:30:47 +0200

    Migliorata pagina live

--- src/routes/liveSession/+page.svelte
+++ src/routes/liveSession/+page.svelte
@@ -6,6 +6,7 @@
 	 */
 	let readings = $state([]);
 	let localIP = $state('');
+	let wsPort = $state('');
 
 	/**
 	 * @type WebSocket | undefined
@@ -17,19 +18,38 @@
 
 	// $inspect(sock);
 	onMount(() => {
-		const host = '192.168.1.105';
-		const port = '3000';
-		sock = new WebSocket('ws://' + host + ':' + port); //use wss for TLS channel
-		console.log(sock);
-		sock.onopen = function (event) {
-			console.log('Connected to WebSocket server.');
-		};
-		sock.onmessage = function (event) {
-			console.log('Received: ' + event.data);
-			readings.push(event.data);
-		};
-		return disconnect; //disconnect as a cleanup
+		//const host = '192.168.1.105';
+		//const port = '3000';
+		//sock = new WebSocket('ws://' + host + ':' + port); //use wss for TLS channel
+		//console.log(sock);
+		//sock.onopen = function (event) {
+		//	console.log('Connected to WebSocket server.');
+		//};
+		//sock.onmessage = function (event) {
+		//	console.log('Received: ' + event.data);
+		//	readings.push(event.data);
+		//};
+		let previousWsInfo = sessionStorage.getItem('wsInfo');
+		if (previousWsInfo){
+			let info = JSON.parse(previousWsInfo)
+			localIP = info.ip;
+			wsPort = info.port
+		}
+		return cleanup; //save ws info and disconnect as a cleanup
 	});
+
+	function cleanup(){
+		saveAddress();
+		disconnect();
+	}
+
+	function saveAddress(){
+		let wsInfo = {
+			ip: localIP,
+			port: wsPort
+		}
+		sessionStorage.setItem('wsInfo', JSON.stringify(wsInfo))
+	}
 
 	function disconnect() {
 		console.log('closing connection');
@@ -37,9 +57,13 @@
 	}
 
 	function connect() {
-		const port = '3000';
-		sock = new WebSocket('ws://' + localIP + ':' + port); //use wss for TLS channel
-		console.log(sock);
+		// default IP 192.168.1.105
+		//dfeault port 3000
+		if (wsPort === '') {
+			wsPort = '3000';
+		}
+		sock = new WebSocket('ws://' + localIP + ':' + wsPort); //use wss for TLS channel
+		// console.log(sock);
 		sock.onopen = function (event) {
 			console.log('Connected to WebSocket server.');
 		};
@@ -54,10 +78,10 @@
 	 *@returns {string}
 	 */
 	function resolveSockState(sock) {
-		console.log(sock?.readyState);
+		// console.log(sock?.readyState);
 		switch (sock?.readyState) {
 			case 0:
-				return 'connecting';
+				return 'attempting to connect';
 			case 1:
 				return 'open';
 			case 2:
@@ -65,7 +89,7 @@
 			case 3:
 				return 'closed';
 			default:
-				return '';
+				return 'not started';
 		}
 	}
 </script>
@@ -74,19 +98,46 @@
 	<title>Live Session</title>
 </svelte:head>
 <div class="wrapper">
-	<h1>Session in progress</h1>
-	<div>Weboscket state: {resolveSockState(sock)}</div>
-
-	<div>
-		Readings above treshold:
-		<ul>
-			{#each readings as reading}
-				<li>{reading}</li>
-			{/each}
-		</ul>
+	<section class="ws-info">
+		<h1>Session in progress</h1>
+		<div>Websocket state: {resolveSockState(sock)}</div>
+		<label for="IP-input">Web socket IP:</label>
+		<input type="text" id="IP-input" bind:value={localIP} />
+		<label for="port-input">Web socket port:</label>
+		<input type="text" id="port-input" bind:value={wsPort} />
+		<button onclick={connect}>Connect WS</button>
+		<button onclick={disconnect}>Disconnect WS</button>	
+	</section>
+	<main class="stats-dashboard">
+		<div class="best">Your best punch this session was:</div>
+		<div class="readings-feed">
+			Readings above treshold:
+			<ul>
+				{#each readings as reading}
+					<li>{reading}</li>
+				{/each}
+			</ul>
+		</div>
+		<div class="graph">
+			<p>Graph</p>
+		</div>
+		<div>test</div>
+	</main>
+	<div class="dashboard-footer">
+		Rate your tiredness <br>
+		Time since session start:
 	</div>
-	<!-- <input type="text" value:bind={localIP}>
-<button onclick={connect}>connect WS</button>
-<div>current socket status: {sockStatus}</div> -->
-	<button onclick={disconnect}>Disconnect WS</button>
 </div>
+
+<style>
+	.ws-info {
+		display: flex;
+		flex-wrap: wrap;
+		/* gap: 20px; */
+		justify-content: space-around;
+		}
+	.stats-dashboard {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+	}
+</style>
