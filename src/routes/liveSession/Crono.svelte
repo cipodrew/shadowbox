baa1f22f5e497c52402af8abd380d7f6ea5a6362
adolci <141414129+cipodrew@users.noreply.github.com>
2024-08-12 23:37:51 +0200

    piccolo cronometro funzionante

--- src/routes/liveSession/Crono.svelte
+++ src/routes/liveSession/Crono.svelte
@@ -1,1 +1,24 @@
-<div>00:00</div>
+<script>
+	let time = $state(0);
+	let hours = $derived(Math.floor(time / (60 * 60)));
+	let minutes = $derived(Math.floor(time / 60));
+	let seconds = $derived(time % 60);
+	setInterval(() => {
+		time++;
+	}, 1000);
+</script>
+
+<div class="crono">
+	<div>seconds passed:{time}</div>
+	<div>{hours}hh:{minutes}mm:{seconds}ss</div>
+</div>
+
+<style>
+	.crono {
+		display: flex;
+		flex-direction: nowrap;
+		gap: 20px;
+
+		font-size: var(--fs-800);
+	}
+</style>
